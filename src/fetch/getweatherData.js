c1fa22e512f63f57bfb01750e13aa719f5dfe924
sethuvicky <75525385+sethuvicky@users.noreply.github.com>
2023-03-29 08:44:36 +0530

    Update getweatherData.js

--- src/fetch/getweatherData.js
+++ src/fetch/getweatherData.js
@@ -3,7 +3,6 @@ import { DateTime } from "luxon";
 const API_KEY = "52a42084b383e2c750ed209fef02096f";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
-// https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
 
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
